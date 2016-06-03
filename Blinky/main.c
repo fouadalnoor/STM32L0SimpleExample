eb4cb9534df62cc8c5d231ee3c994c21df2ef7a5
Fouad Al-Noor <fouadalnoor@hotmail.com>
2016-06-03 07:39:37 -0700

    added manual bit manipulation of registers in main.c

--- Blinky/main.c
+++ Blinky/main.c
@@ -3,22 +3,45 @@
 #include "Board_Buttons.h"
 
 int main(){
-	//initilise LED's 
+	//initilise LED's using API
+	//LED_Initialize();
 	
-	LED_Initialize();
+	//Manually initilising the registers 
+	
+	//Enable the GPIOA and GPIOB clocks (to enable those peripherals) 
+	unsigned long int initLED = ((1<< 0UL)|(1 << 1UL)); //in binary
+	unsigned long int initLEDh = 0x3; //in hex
+	RCC->IOPENR |= initLED;
+	
+	//Enable the GPIOA pin 3 and GPIOB pin 4 as Outputs (push-pull) 
+	//GPIOA->MODER &= ~((3UL << 2*5)); //?
+	
+	GPIOA->MODER |= (1UL<< 10);// Set bit number 10;
+	GPIOA->MODER &= ~(1UL<< 11);// unset bit number 11;
+	
+	//OTYPER Could set the output type here to push-pull, but it's that by default so no need. 
+	
+	//setting the speed to me medium 
+	GPIOA->OSPEEDR |= (1UL<< 10);
+	GPIOA->OSPEEDR &= ~(1UL<< 11);
+
+//Set no pull-up or pull down as 
+	GPIOA->PUPDR &= ~(1UL<< 10); //unset bit number 10
+	GPIOA->PUPDR &= ~(1UL<< 11); //unset bit number 11
 	
 	//initilise buttons
-	Buttons_Initialize();
+	//Buttons_Initialize();
 	
-	//Turn on  RED LED
+	//Turn on  Green LED
+	GPIOA->BSRR |= (1UL<< 5); //Set Bit number 5 (i.e GPIOA pin 5). 
+	//LED_Off(0);
 	
-	LED_Off(0);
-	
-	//Turn on Green LED  
-	LED_Off(1);
+	//Turn on Red LED  
+	//LED_Off(1);
 	
 while(1){
 	
+	/*
 	if(Buttons_GetState())
 	{
 		//turn on the LED's
@@ -32,7 +55,11 @@ while(1){
 		LED_Off(0);
 		LED_Off(1);
 	}
+	
+	*/
 }
+	
+	
 	
 	//return 0; 
 }
