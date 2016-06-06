5c9ca54839218b1c8a97ec8465e913ae53c36e2b
Fouad Al-Noor <fouadalnoor@hotmail.com>
2016-06-06 07:01:16 -0700

    changed main.c in order to only use the green led, using api's

--- Blinky/main.c
+++ Blinky/main.c
@@ -4,47 +4,47 @@
 
 int main(){
 	//initilise LED's using API
-	//LED_Initialize();
+	LED_Initialize();
 	
 	//Manually initilising the registers 
 	
 	//Enable the GPIOA and GPIOB clocks (to enable those peripherals) 
-	unsigned long int initLED = ((1<< 0UL)|(1 << 1UL)); //in binary
-	unsigned long int initLEDh = 0x3; //in hex
-	RCC->IOPENR |= initLED;
+	//unsigned long int initLED = ((1<< 0UL)|(1 << 1UL)); //in binary
+	//unsigned long int initLEDh = 0x3; //in hex
+	//RCC->IOPENR |= initLED;
 	
 	//Enable the GPIOA pin 3 and GPIOB pin 4 as Outputs (push-pull) 
 	//GPIOA->MODER &= ~((3UL << 2*5)); //?
 	
-	GPIOA->MODER |= (1UL<< 10);// Set bit number 10;
-	GPIOA->MODER &= ~(1UL<< 11);// unset bit number 11;
+	//GPIOA->MODER |= (1UL<< 10);// Set bit number 10;
+	//GPIOA->MODER &= ~(1UL<< 11);// unset bit number 11;
 	
 	//OTYPER Could set the output type here to push-pull, but it's that by default so no need. 
 	
 	//setting the speed to me medium 
-	GPIOA->OSPEEDR |= (1UL<< 10);
-	GPIOA->OSPEEDR &= ~(1UL<< 11);
+	//GPIOA->OSPEEDR |= (1UL<< 10);
+	//GPIOA->OSPEEDR &= ~(1UL<< 11);
 
 //Set no pull-up or pull down
-	GPIOA->PUPDR &= ~(1UL<< 10); //unset bit number 10
-	GPIOA->PUPDR &= ~(1UL<< 11); //unset bit number 11
+	//GPIOA->PUPDR &= ~(1UL<< 10); //unset bit number 10
+	//GPIOA->PUPDR &= ~(1UL<< 11); //unset bit number 11
 	
 	// Automatically initilise buttons
-	//Buttons_Initialize();
+	Buttons_Initialize();
 	
 	//Manually 
 	//Push button connected to PA0. 
 	
-	GPIOA->MODER &= ~(1UL<<0);//unset bit number 0
-	GPIOA->MODER &= ~(1UL<<1);//unset bit number 1 
+	//GPIOA->MODER &= ~(1UL<<0);//unset bit number 0
+	//GPIOA->MODER &= ~(1UL<<1);//unset bit number 1 
 	
 	//Set no pull-up or pull down
-	GPIOA->PUPDR &= ~(1UL<<0); //unset bit number 10
-	GPIOA->PUPDR &= ~(1UL<<1); //unset bit number 11
+	//GPIOA->PUPDR &= ~(1UL<<0); //unset bit number 10
+	//GPIOA->PUPDR &= ~(1UL<<1); //unset bit number 11
 	
 	
 	//Turn on  Green LED
-	GPIOA->BSRR |= (1UL<< 5); //Set Bit number 5 (i.e GPIOA pin 5).
+	//GPIOA->BSRR |= (1UL<< 5); //Set Bit number 5 (i.e GPIOA pin 5).
 
  
 	//LED_Off(0);
@@ -55,11 +55,12 @@ int main(){
 while(1){
 	
 	//Read the Input data register 
-	 int InputDataReg = GPIOA->IDR; 
+	 //int InputDataReg = GPIOA->IDR; 
 	
 	// Turn off LED
 	//GPIOA->BSRR &= ~ (1UL<< 5); //unset Bit number 5 (i.e GPIOA pin 5). 
 	
+	/*
 	if(((InputDataReg & (1UL<<0)) != 0))
 	{
 		
@@ -82,6 +83,17 @@ while(1){
 		//Turn off the LED's
 		//LED_Off(0);
 		//LED_Off(1);
+	}
+	
+	*/
+	
+	if(Buttons_GetState()) {
+		
+		LED_On(0);
+	}
+	
+	else {
+		LED_Off(0);
 	}
 	
 	
